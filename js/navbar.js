4918d3ee15dbfd26702c86404fd23477c6de1ef7
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-24 17:25:03 +0700

    Modif Navbar

---
+++ js/navbar.js
@@ -0,0 +1,31 @@
+const navbar = document.getElementById('navbar');
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('shadow-lg');
+    } else {
+        navbar.classList.remove('shadow-lg');
+    }
+});
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("text-blue", "border-b-[3px]");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("text-blue", "border-b-[3px]");
+    } else {
+      link.classList.add("text-black");
+    }
+  });
+});
