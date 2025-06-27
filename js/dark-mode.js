1e5395600b300182f592b4bea4f12ee18781b68f
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-27 10:19:02 +0700

    Add Dark Mode to LocalStorage

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,6 +1,12 @@
 const toggle = document.getElementById('dark-mode-toggle');
-const html = document.getElementById('htmlRoot')
+const html = document.documentElement;
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+  html.classList.add('dark');
+}
 
 toggle.addEventListener('click', () => {
     html.classList.toggle('dark');
+    localStorage.setItem('theme', html.classList.contains('dark') ? 'dark' : 'light');
 });
